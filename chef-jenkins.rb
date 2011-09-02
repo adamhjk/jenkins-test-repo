2f38e0309cd20bb2a116122828956eb2e20ac92d
Adam Jacob <adam@opscode.com>
2011-09-01 21:45:04 -0700

    Updating the path to my key

--- chef-jenkins.rb
+++ chef-jenkins.rb
@@ -2,7 +2,7 @@ current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                "adam"
-client_key               "/Users/adam/src/sandbox/opscode/environmetns/opscode-org/chef-repo/.chef/adam.pem"
+client_key               "/Users/adam/src/sandbox/opscode/environments/opscode-org/chef-repo/.chef/adam.pem"
 chef_server_url          "https://api.opscode.com/organizations/opscode"
 cache_type               'BasicFile'
 cache_options( :path => "/tmp/.chef/checksums" )
